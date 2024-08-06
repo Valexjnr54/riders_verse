11b24a67919d426bb88a87e897e3e25176a9840a
Valexjnr54 <jimvalex54@gmail.com>
2024-08-06 08:24:16 +0100

    Fixing Accept Delivery for Rider

--- src/routes/Riders/deliverRoutes.ts
+++ src/routes/Riders/deliverRoutes.ts
@@ -1,10 +1,12 @@
 // src/routes/userRoutes.ts
 import express from 'express';
 import { authenticateJWT } from '../../middlewares/authMiddleware/authenticationMiddleware';
-import { pickDelivery, viewAllDelivery, viewSingleDelivery } from '../../controllers/RiderControllers/deliveryController';
+import { acceptDelivery, pickDelivery, rejectDelivery, viewAllDelivery, viewSingleDelivery } from '../../controllers/RiderControllers/deliveryController';
 
 export const riderDeliveryRouter = express.Router();
 
 riderDeliveryRouter.get('/pickup-delivery', authenticateJWT, pickDelivery);
 riderDeliveryRouter.get('/all-delivery', authenticateJWT, viewAllDelivery);
 riderDeliveryRouter.get('/single-delivery', authenticateJWT, viewSingleDelivery);
+riderDeliveryRouter.put('/accept-delivery', authenticateJWT, acceptDelivery);
+riderDeliveryRouter.put('/reject-delivery', authenticateJWT, rejectDelivery);
